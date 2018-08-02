4a761d40fc971935d78c9873f756919744cb9269
zhm <frontzhm@163.com>
2018-08-02 10:05:59 +0800

    add broadcast

--- utils.js
+++ utils.js
@@ -23,3 +23,28 @@ function parseURL(url) {
   }
 }
 
+// 依赖parseURL,直接得到某key
+function getQuery(url,key){
+  return parseURL(url).query.key
+}
+
+// rem
+function rem() {
+  var docEl = document.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function() {
+      var clientWidth = docEl.clientWidth;
+      if (!clientWidth) return;
+      docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+      if (clientWidth > 750) {
+        docEl.style.width = "750px";
+        docEl.style.marginLeft = "auto";
+        docEl.style.marginRight = "auto";
+        docEl.style.fontSize = "150px";
+      }
+    };
+  if (!document.addEventListener) return;
+  window.addEventListener(resizeEvt, recalc, false);
+  document.addEventListener('DOMContentLoaded', recalc, false);
+}
+
